de39e6454b9dc2ccac5cf96af576d373b5b32e9a
khs <kmin135@naver.com>
2022-08-06 11:30:59 +0900

    QType 기본

--- src/test/java/study/querydsl/QueryDslBasicTest.java
+++ src/test/java/study/querydsl/QueryDslBasicTest.java
@@ -1,7 +1,6 @@
 package study.querydsl;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +12,8 @@ import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static study.querydsl.entity.QMember.member;
 
 @SpringBootTest
 @Transactional
@@ -54,12 +54,27 @@ public class QueryDslBasicTest {
 
     @Test
     public void startQueryDsl() {
+        // 여기에 지정한 이름을 JPQL의 alias로 쓴다.
+        // 같은 테이블을 조인할 때는 다른 이름의 QType 들을 만들어서 하면 된다.
+        // 그 외에는 가독성을 위해 startQueryDsl2() 처럼 static import 해서 사용하자
         QMember m = new QMember("m");
 
         Member findMember = query
                 .select(m)
                 .from(m)
                 .where(m.username.eq("member1"))
+                .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void startQueryDsl2() {
+        // 이처럼 Q Type에 static 정의된 alias를 사용하면 코드가 심플해진다.
+        Member findMember = query
+                .select(member)
+                .from(member)
+                .where(member.username.eq("member1"))
                 .fetchOne();
 
         assertThat(findMember.getUsername()).isEqualTo("member1");
