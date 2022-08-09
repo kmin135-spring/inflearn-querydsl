eb4e87d32fe45a43a3d1ee520038bc4404478bf4
khs <kmin135@naver.com>
2022-08-09 20:41:30 +0900

    동적쿼리 - BooleanBuilder

--- src/test/java/study/querydsl/QueryDslBasicTest.java
+++ src/test/java/study/querydsl/QueryDslBasicTest.java
@@ -1,5 +1,6 @@
 package study.querydsl;
 
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.ExpressionUtils;
@@ -688,4 +689,32 @@ public class QueryDslBasicTest {
                 .fetch();
         result.forEach(System.out::println);
     }
+
+    /*
+    동적 쿼리
+     */
+
+    @Test
+    void dynamicQueryBooleanBuilder() {
+        String usernameParam = "member1";
+        Integer ageParam = null;
+
+        List<Member> result = searchMember1(usernameParam, ageParam);
+        assertThat(result.size()).isEqualTo(1);
+    }
+
+    private List<Member> searchMember1(String usernameCond, Integer ageCond) {
+        BooleanBuilder builder = new BooleanBuilder();
+        if(usernameCond != null) {
+            builder.and(member.username.eq(usernameCond));
+        }
+        if(ageCond != null) {
+            builder.and(member.age.eq(ageCond));
+        }
+
+        return query.selectFrom(member)
+                .where(builder)
+                .fetch();
+    }
+
 }
